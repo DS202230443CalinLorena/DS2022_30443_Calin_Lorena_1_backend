5b3d87d3acd977fe03bab547b9758d843f8db740
Lorena Laura Calin <Calin.Ra.Lorena@student.utcluj.ro>
2023-01-08 01:45:41 +0200

    added MessageHandler

--- src/main/java/com/example/assignment1backend/service/ChatService.java
+++ src/main/java/com/example/assignment1backend/service/ChatService.java
@@ -1,18 +1,38 @@
 package com.example.assignment1backend.service;
 
-import com.thetransactioncompany.jsonrpc2.JSONRPC2Parser;
+import com.example.assignment1backend.chat.MessageHandler;
+import com.thetransactioncompany.jsonrpc2.*;
 import com.thetransactioncompany.jsonrpc2.server.Dispatcher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
-
-import javax.websocket.MessageHandler;
 
 @Service
 public class ChatService {
+    private SimpMessagingTemplate simpMessagingTemplate;
     private Dispatcher dispatcher;
-    private MessageHandler messageHandler;
     private JSONRPC2Parser jsonrpc2Parser;
+    private MessageHandler messageHandler;
 
     @Autowired
-    public ChatService
+    public ChatService(SimpMessagingTemplate simpMessagingTemplate){
+        this.simpMessagingTemplate = simpMessagingTemplate;
+        this.dispatcher = new Dispatcher();
+        this.messageHandler = new MessageHandler();
+        this.jsonrpc2Parser = new JSONRPC2Parser();
+        dispatcher.register(messageHandler);
+    }
+
+    public JSONRPC2Response sendMessage(String request){
+        JSONRPC2Request req;
+        try{
+            req = this.jsonrpc2Parser.parseJSONRPC2Request(request);
+            System.out.println("Method:" + req.getMethod());
+            System.out.println("Req:" + req);
+            return dispatcher.process(req, null);
+        } catch (JSONRPC2ParseException e){
+            e.printStackTrace();
+        }
+        return new JSONRPC2Response(JSONRPC2Error.METHOD_NOT_FOUND, null);
+    }
 }
